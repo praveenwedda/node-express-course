296a488b09cb88eaa271739f711c2cc07ee8b2c9
praveenwedda <praveenwedda1992@gmail.com>
2024-06-23 12:19:47 +0530

    basic example of server setup done

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+console.log("Express Tutorial");
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About page</h1>");
+    res.end();
+  } else if (url === "/contact") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Contact page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Not found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
