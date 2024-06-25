35895fb18bb49e6c36cf1e237f0ab91ee57e0563
Praveen W <praveenwedda1992@gmail.com>
2024-06-25 07:26:20 -0500

    converted the entire nav bar app to an express app via app.js, introduced a public folder to maintain all resources

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,22 @@
 console.log("Express Tutorial");
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require("path");
 
+
+app.use(express.static('./public'))
 
 app.get('/', (req,res)=>{
-  res.send("Home page")
+  res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
 })
 
 app.get('/about', (req,res)=>{
-  res.status(404).send("About us page")
+  res.send("About us page")
 })
 
 app.all('*', (req,res)=>{
-  res.send("resource not found")
+  res.status(404).send("resource not found")
 })
 
 app.listen(5000, ()=>{
