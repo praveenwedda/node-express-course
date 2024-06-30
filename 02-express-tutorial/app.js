d994ee2b3b72eb1f814c0d1bf300f64bbdb43105
praveenwedda <praveenwedda1992@gmail.com>
2024-06-30 12:47:18 +0530

    commit of creating simplest form of API to fetch info from a data.js file that has data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,22 +2,22 @@ console.log("Express Tutorial");
 
 const express = require("express");
 const app = express();
-const path = require("path");
+const { products } = require("./data");
 
-app.use(express.static("./public"));
-
-// app.get('/', (req,res)=>{
-//   res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-// })
-
-app.get("/about", (req, res) => {
-  res.send("About us page");
+app.get("/", (req, res) => {
+  res.send("<h1>Home page</h1><a href = /api/products/1>click me</a>");
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/products/1", (req, res) => {
+  // const newProducts = products.map((product) => {
+  //   const { id, name, image } = product;
+  //   return { id, name, image };
+  // });
+  const singleProduct = products.find((product) => product.id === 1);
+
+  res.json(singleProduct);
 });
 
 app.listen(5000, () => {
-  console.log("server listening on port 5000");
+  console.log("server listening to port 5000");
 });
