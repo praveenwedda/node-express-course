355fe900bbcacf54424b11238ec2ca431a50f3ec
praveenwedda <praveenwedda1992@gmail.com>
2024-06-30 12:59:22 +0530

    updated the api to be able to dynamically pick data based on product ID

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -8,12 +8,17 @@ app.get("/", (req, res) => {
   res.send("<h1>Home page</h1><a href = /api/products/1>click me</a>");
 });
 
-app.get("/api/products/1", (req, res) => {
+app.get("/api/products/:productID", (req, res) => {
   // const newProducts = products.map((product) => {
   //   const { id, name, image } = product;
   //   return { id, name, image };
   // });
-  const singleProduct = products.find((product) => product.id === 1);
+  console.log(req.params);
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
 
   res.json(singleProduct);
 });
