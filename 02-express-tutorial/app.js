4618f0dc65c1b6a4c699fda8c7398558a172aade
praveenwedda <praveenwedda1992@gmail.com>
2024-07-07 23:03:49 +0530

    How POST method works was not understood fully. More than anything how did app.use with the folder up the expected html project? needs relooking

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,6 +2,24 @@ console.log("app.js running...");
 const express = require("express");
 const app = express();
 
+let { people } = require("./data");
+
+app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send(`Please provide a name`);
+  res.send("POST");
+});
+
 app.listen(5000, () => {
   console.log("server listening to port 5000");
 });
